5871a3d5538c9b8e1e365086abdaedb089ff5fee
BayHen <ngobayhen.tdc2019@gmail.com>
2021-10-26 21:24:38 +0700

    Make Detail

--- wp-content/themes/twentytwenty/template-parts/content.php
+++ wp-content/themes/twentytwenty/template-parts/content.php
@@ -13,44 +13,74 @@
 
 ?>
 
-<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+<!-- Bootstrap CSS -->
+<?php
+if (!is_single()) :
+?>
+	<div class="col-md-8">
+		<div class="list_new_view">
+			<div class="row">
+				<?php if (is_search()) : ?>
+					<div class="col-md-3">
+						<div class="top_news_block_thumb">
+							<img src="<?= get_the_post_thumbnail_url($post) ?>">
+						</div>
+					</div>
+				<?php endif; ?>
+				<div class="col-md-7 top_news_block_desc">
+					<div class="row">
+						<div class="col-md-3 col-xs-3 topnewstime">
+							<span class="topnewsdate"><?php echo date('d', strtotime($post->post_date)) ?></span><br>
+							<span class="topnewsmonth">Tháng <?php echo date('m', strtotime($post->post_date)) ?></span><br>
+						</div>
+						<div class="col-md-9 col-xs-9 shortdesc">
+							<?php the_title('<h4><a href="' . esc_url(get_permalink()) . '">', '</a></h4>'); ?>
+							<p>
+								<?php echo strip_tags(substr($post->post_content, 0, 300));	?>
+								<a href="<?= esc_url(get_permalink()) ?>">[...]</a>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
-	<?php
+<?php else : ?>
 
-	get_template_part( 'template-parts/entry-header' );
-
-	if ( ! is_search() ) {
-		get_template_part( 'template-parts/featured-image' );
-	}
-
-	?>
-
-	<div class="post-inner <?php echo is_page_template( 'templates/template-full-width.php' ) ? '' : 'thin'; ?> ">
-
-		<div class="entry-content">
-
+	<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+		<div class="col-md-8">
 			<?php
-			if ( is_search() || ! is_singular() && 'summary' === get_theme_mod( 'blog_content', 'full' ) ) {
-				the_excerpt();
-			} else {
-				the_content( __( 'Continue reading', 'twentytwenty' ) );
+
+			get_template_part('template-parts/entry-header');
+
+			if (!is_search()) {
+				get_template_part('template-parts/featured-image');
 			}
+
 			?>
-
-		</div><!-- .entry-content -->
-
-	</div><!-- .post-inner -->
-
-	<div class="section-inner">
-		<?php
-		wp_link_pages(
-			array(
-				'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__( 'Page', 'twentytwenty' ) . '"><span class="label">' . __( 'Pages:', 'twentytwenty' ) . '</span>',
-				'after'       => '</nav>',
-				'link_before' => '<span class="page-number">',
-				'link_after'  => '</span>',
-			)
-		);
+			<div class="row maincontent">
+				<div class="col-md-12">
+					<?php
+					if (is_search() || !is_singular() && 'summary' === get_theme_mod('blog_content', 'full')) {
+						the_excerpt();
+					} else {
+						the_content(__('Continue reading', 'twentytwenty'));
+					}
+					?>
+				</div>
+			</div>
+		</div>
+		<div class="section-inner">
+			<?php
+			wp_link_pages(
+				array(
+					'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__('Page', 'twentytwenty') . '"><span class="label">' . __('Pages:', 'twentytwenty') . '</span>',
+					'after'       => '</nav>',
+					'link_before' => '<span class="page-number">',
+					'link_after'  => '</span>',
+				)
+			);
 
 		edit_post_link();
 
@@ -88,7 +118,8 @@
 		</div><!-- .comments-wrapper -->
 
 		<?php
-	}
-	?>
+		}
+		?>
+	</article><!-- .post -->
 
-</article><!-- .post -->
+<?php endif; ?>
