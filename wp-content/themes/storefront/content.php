6c9c8979b7bd3f55977b6192ef1ab19c7b1ad347
BayHen <ngobayhen.tdc2019@gmail.com>
2021-12-06 23:38:32 +0700

    Update blog

--- wp-content/themes/storefront/content.php
+++ wp-content/themes/storefront/content.php
@@ -5,7 +5,7 @@
  *
  * @package storefront
  */
-
+$post = get_post();
 ?>
 
 
@@ -13,17 +13,21 @@
 <div class="fr-contents-card full">
 	<div class="fr-spacer l"></div>
 		<div>
-			<div id="" class="fr-list mt-xxs mbt-xxs mb-xxs bordered-between-content"><a href="https://www.uniqlo.com/vn/vi/news/topics/2021101901/" target="_self" class="">
+			<div id="" class="fr-list mt-xxs mbt-xxs mb-xxs bordered-between-content">
 					<div class="fr-link-wrapper block">
 						<div class="fr-image-plus-text left">
-							<div class="image"><img src="//im.uniqlo.com/global-cms/spa/rese1ec9c647e0afe41eada1ae669695e79fr.jpg" alt="Eight Clean-Look Sweats"></div>
+							<div class="image">
+								<a href="<?php the_permalink(); ?>">
+									<?php the_post_thumbnail('medium'); ?>
+								</a>
+							</div>
 							<div class="info">
 								<div class="title"><?php do_action('storefront_loop_post');?></div>
-								<div class="description"><?php do_action('storefront_loop_post_content');?></div>
+								<div class="description"><?php echo substr($post -> post_content,0,90)?></div>
 							</div>
 						</div>
 					</div>
-				</a></div>
+				</div>
 		</div>
 	<div class="fr-spacer l"></div>
 </div>	
