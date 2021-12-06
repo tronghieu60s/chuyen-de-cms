26d7fdf11e70cbed78f6c3004dd208ddb66cd7c9
BayHen <ngobayhen.tdc2019@gmail.com>
2021-12-06 22:23:59 +0700

    Make blog

--- wp-content/themes/storefront/content.php
+++ wp-content/themes/storefront/content.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template used to display post content.
  *
@@ -7,17 +8,24 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<?php
-	/**
-	 * Functions hooked in to storefront_loop_post action.
-	 *
-	 * @hooked storefront_post_header          - 10
-	 * @hooked storefront_post_content         - 30
-	 * @hooked storefront_post_taxonomy        - 40
-	 */
-	do_action( 'storefront_loop_post' );
-	?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<div class="fr-contents-card full">
+	<div class="fr-spacer l"></div>
+		<div>
+			<div id="" class="fr-list mt-xxs mbt-xxs mb-xxs bordered-between-content"><a href="https://www.uniqlo.com/vn/vi/news/topics/2021101901/" target="_self" class="">
+					<div class="fr-link-wrapper block">
+						<div class="fr-image-plus-text left">
+							<div class="image"><img src="//im.uniqlo.com/global-cms/spa/rese1ec9c647e0afe41eada1ae669695e79fr.jpg" alt="Eight Clean-Look Sweats"></div>
+							<div class="info">
+								<div class="title"><?php do_action('storefront_loop_post');?></div>
+								<div class="description"><?php do_action('storefront_loop_post_content');?></div>
+							</div>
+						</div>
+					</div>
+				</a></div>
+		</div>
+	<div class="fr-spacer l"></div>
+</div>	
 
 </article><!-- #post-## -->
